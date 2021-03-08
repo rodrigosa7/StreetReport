4c81cc0af8b536aa1eaad22558637f2bb6474787
rodrigosa7 <digex2008@live.com.pt>
2021-03-08 21:29:27 +0000

    Criei o NoteDB.kt

--- app/src/main/java/ipvc/estg/streetreport/Notes.kt
+++ app/src/main/java/ipvc/estg/streetreport/Notes.kt
@@ -12,18 +12,22 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class Notes : AppCompatActivity() {
+    /*
     private lateinit var myList: ArrayList<Note>
-
+    */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
-
+        /*
         myList = ArrayList<Note>()
 
         for(i in 0 until 100){
             myList.add(Note("Nota $i", "Descrição $i", Date()))
         }
+         */
+        /*
         recycler_view.adapter = LineAdapter(myList)
+         */
         recycler_view.layoutManager = LinearLayoutManager(this)
     }
 
---
+++ app/src/main/java/ipvc/estg/streetreport/db/NoteDB.kt
@@ -0,0 +1,73 @@
+package ipvc.estg.streetreport.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import ipvc.estg.streetreport.dao.NotesDao
+import ipvc.estg.streetreport.entities.Note
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.util.*
+
+@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
+public abstract class NoteDB : RoomDatabase() {
+// Annotates class to be a Room Database with a table (entity) of the Word class
+
+    abstract fun notesDao(): NotesDao
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    var noteDao = database.notesDao()
+
+                    // Delete all content here.
+                    noteDao.deleteAll()
+
+                    // Add sample cities.
+                    var note = Note(1, "Nota 1", "Lombas", Date())
+                    noteDao.insert(note)
+                    note = Note(2, "Note 2", "Buracos", Date())
+                    noteDao.insert(note)
+                    note = Note(3, "Note 3", "Pouca Luz", Date())
+                    noteDao.insert(note)
+
+                }
+            }
+        }
+    }
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NoteDB? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): NoteDB {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDB::class.java,
+                    "notes_database"
+                )
+                    //estratégia de destruição
+                    .fallbackToDestructiveMigration()
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
